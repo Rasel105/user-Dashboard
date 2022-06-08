975a870f1885ad2a09f3fe0dae2860103f5adadc
Azadul Islam Rasel <azad.is.rasel@gmail.com>
2022-06-08 23:49:42 +0600

    register page created

--- src/components/Login/Register/Register.js
+++ src/components/Login/Register/Register.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Register = () => {
     return (
         <div className='flex h-screen justify-center items-center'>
-            <div className="card w-96 bg-base-100 shadow-xl mt-20">
+            <div className="card w-96 bg-base-100 shadow-xl mt-14">
                 <div className="card-body">
                     <h2 className="text-center text-2xl font-bold">Register</h2>
                     <form >
@@ -39,9 +39,9 @@ const Register = () => {
                                 className="input input-bordered w-full max-w-xs"
                             />
                         </div>
-                        <input className="btn w-full btn-primary uppercase font-bold text-white bg-gradient-to-r from-secondary to-primary" type="submit" value="Login" />
+                        <input className="btn w-full btn-primary uppercase font-bold text-white bg-gradient-to-r from-secondary to-primary" type="submit" value="Register" />
                     </form>
-                    <p className='text-center'><small>New to SyntiqHub Portal? <Link to={"/login"} className="text-secondary">Create New Account</Link></small></p>
+                    <p className='text-center'><small>Already have SyntiqHub Portal? <Link to={"/login"} className="text-secondary">Login</Link></small></p>
                     <div className="divider">OR</div>
                     <button
                         // onClick={() => signInWithGoogle()}
